78cae6756972d22b3175ad9f818461cfa371a4bb
nemisgerry <gerrymaths@gmail.com>
2016-08-19 00:12:58 -0500

    Update cuadro magico

--- cuadro_magico.py
+++ cuadro_magico.py
@@ -1,4 +1,4 @@
-def imprimir_cuadro(cuadro_magico):
+def imprimir_cuadro():
 	for renglon in cuadro_magico:
 		print renglon
 
@@ -29,23 +29,6 @@ def verificar_columnas():
 	return True
 
 
-#Crear una matriz que represente el cuadro magico
-tamanio=int(raw_input("Dame el tamanio de tu cuadro magico: "))
-#cuadro_magico=[[0 for x in range (tamanio)] for y in range(tamanio)]
-cuadro_magico=[[1,2,3],[4,5,6],[7,8,9]]
-
-#Cuadro magico parcial
-#cuadro_magico=[[4,9,0],[0,0,0],[0,0,0]]
-
-#Cuadro magico valido
-#cuadro_magico=[[4,9,2],[3,5,7],[8,1,6]]
-
-ncuadrados_totales=tamanio**2
-ncuadrados_posibles=[True for x in range (ncuadrados_totales)]
-
-#Referencia a este valor en: http://mathworld.wolfram.com/MagicSquare.html
-constante_magica=((tamanio**3)+(tamanio))/2
-
 
 def verificar_diagonales():
 	sin_llenar=False
@@ -56,10 +39,47 @@ def verificar_diagonales():
 			sin_llenar=True
 	if (sin_llenar==False and constante_acumulada!=constante_magica):
 		return False
-	constante_acumulada=0
 	
+	constante_acumulada=0
 	for pos in range(tamanio-1,-1,-1):
 		constante_acumulada+=cuadro_magico[tamanio-1-pos][pos]
-		if cuadro_magico[tamanio-1-pos][pos]==0
-imprimir_cuadro(cuadro_magico)
-verificar_diagonales()
+		if cuadro_magico[tamanio-1-pos][pos]==0:
+			sin_llenar==True
+	if (sin_llenar==False and constante_acumulada!=constante_magica):
+		return False
+	return True	
+
+def back_magic(renglon,columna):
+	if (not verificar_renglones() or not verificar_columnas() or not verificar_diagonales()):
+		return
+	if (renglon==tamanio):
+		imprimir_cuadro()
+		print "*******************"
+		return
+	for i in range(0,ncuadrados_totales):
+		if ncuadrados_posibles[i]==True:
+			cuadro_magico[renglon][columna]=i+1
+			ncuadrados_posibles[i]=False
+
+			sig_columna=columna+1
+			sig_renglon=renglon
+			if sig_columna==tamanio:
+				sig_renglon+=1
+				sig_columna=0
+
+			back_magic(sig_renglon,sig_columna)
+
+			cuadro_magico[renglon][columna]=0
+			ncuadrados_posibles[i]=True
+
+
+
+
+#Referencia a este valor en: http://mathworld.wolfram.com/MagicSquare.htm
+
+tamanio=int(raw_input("Dame el tamanio de tu cuadro magico: "))
+cuadro_magico=[[0 for x in range (tamanio)] for y in range(tamanio)]
+ncuadrados_totales=tamanio**2
+ncuadrados_posibles=[True for x in range (ncuadrados_totales)]
+constante_magica=((tamanio**3)+(tamanio))/2
+back_magic(0,0)
